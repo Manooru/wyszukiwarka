d49b21a3a0f317b78a0ff219a7791e9efd6fc1e3
Manooru <konradklokocki@gmail.com>
2025-01-22 16:32:20 +0100

    a

--- src/Search.java
+++ src/Search.java
@@ -2,18 +2,52 @@ import java.util.Scanner;
 public class Search {
     public static void Search(){
         //przepraszam za spaghetti, które jest w wyszukiwaniu przez gatunek
+            System.out.println("Proszę wybrać następujące gatunki: \n1. Akcja\n2. Komedia\n3. Romantyczne\n\nWpisz 0, aby wyjść z wybrania kategorii i przejść dalej");
+            search2();
+    }
+    public static void search2(){
         Scanner scanner=new Scanner(System.in);
-        int pick=-1;
-        if(filter.hasGenre=true){
-            System.out.println("Proszę wybrać następujące gatunki: \n1. Akcja\n2. Komedia\n3. Romantyczne");
-            while(pick!=0) {
-                pick= scanner.nextInt();
-                if (pick==1){
-                    if(filter.akcja)
+        int pick;
+        pick= scanner.nextInt();
+        switch (pick){
+            case 1:
+                if(filter.akcja=false){
                     filter.akcja=true;
+                }else{
+                    System.out.println("Wyszukiwane przez gatunek akcji jest włączone, czy chcesz wyłączyć?\n1 - tak\n 2 - nie");
+                    pick= scanner.nextInt();
+                    if(pick==1){
+                        filter.akcja=false;
+                    }
                 }
-            }
-        };
-    }else{
+                search2();
+                break;
+            case 2:
+                if(filter.komedia=false){
+                    filter.komedia=true;
+                }else{
+                    System.out.println("Wyszukiwanie przez gatunek komedii jest włączone, czy chcesz wyłączyć?\n1 - tak\n2 - nie");
+                    pick= scanner.nextInt();
+                    if(pick==1){
+                        filter.komedia=false;
+                    }
+                }
+                search2();
+                break;
+            case 3:
+                if(filter.romantyczne=false){
+                    filter.romantyczne=true;
+                }else{
+                    System.out.println("Wyszukiwanie przez gatunek romantyczne jest włączone, czy chcesz wyłaczyć?\n1 - tak\n2 - nie");
+                    pick= scanner.nextInt();
+                    if(pick==1){
+                        filter.romantyczne=false;
+                    }
+                }
+                search2();
+                break;
+            case 0:
+                break;
+        }
     }
 }
